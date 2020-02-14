c894993ab2922bab33a08c7d0cfcd76192f119ac
Vegard Haraldstad <vegard.haraldstad@gmail.com>
2020-02-14 12:59:13 +0100

    Added queue_print_queue() and some documentation

--- source/main.c
+++ source/main.c
@@ -4,6 +4,9 @@
 #include "hardware.h"
 #include "timer.h"
 #include "elevator.h"
+#include "queue.h"
+#include "utilities.h"
+
 
 // int main()
 // {
@@ -28,27 +31,27 @@
 
 // }
 
-static void clear_all_order_lights(){
-    HardwareOrder order_types[3] = {
-        HARDWARE_ORDER_UP,
-        HARDWARE_ORDER_INSIDE,
-        HARDWARE_ORDER_DOWN
-    };
+// static void clear_all_order_lights(){
+//     HardwareOrder order_types[3] = {
+//         HARDWARE_ORDER_UP,
+//         HARDWARE_ORDER_INSIDE,
+//         HARDWARE_ORDER_DOWN
+//     };
 
-    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-        for(int i = 0; i < 3; i++){
-            HardwareOrder type = order_types[i];
-            hardware_command_order_light(f, type, 0);
-        }
-    }
-}
+//     for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+//         for(int i = 0; i < 3; i++){
+//             HardwareOrder type = order_types[i];
+//             hardware_command_order_light(f, type, 0);
+//         }
+//     }
+// }
 
-static void sigint_handler(int sig){
-    (void)(sig);
-    printf("Terminating elevator\n");
-    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-    exit(0);
-}
+// static void sigint_handler(int sig){
+//     (void)(sig);
+//     printf("Terminating elevator\n");
+//     hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+//     exit(0);
+// }
 
 int main(){
     int error = hardware_init();
