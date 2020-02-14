de0838cadf15dec210c2c56ce1f3fbd8081cfebd
Thomas Borge Skøien <thomabsk@stud.ntnu.no>
2020-02-14 10:50:54 +0100

    Updated drivers, and made elevator functions

--- source/elevator.h
+++ source/elevator.h
@@ -8,4 +8,4 @@ void elevator_calibrate();
 int elevator_get_current_floor();
 int elevator_change_floor();
 int elevator_emergency_stop();
-int elevator_wait_and_get_passengers();
+void elevator_wait();
