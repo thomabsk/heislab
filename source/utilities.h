f77279ec62cc88b722d4a779a3f89dc7f1202e3b
Vegard Haraldstad <vegard.haraldstad@gmail.com>
2020-02-14 13:49:15 +0100

    Documented queue.h and utilities.h, fixed bug in queue_is_floor_ordered()

--- source/utilities.h
+++ source/utilities.h
@@ -1,13 +1,28 @@
+/**
+ * @file
+ * @brief Various usefull utillities for elevator project
+ * 
+ */
+
 #ifndef UTILITIES_H
 #define UTILITIES_H
 #define NUMBER_OF_FLOORS 4
 
+/**
+ * @brief Order type used in @c queue_add_floor
+ * 
+ */
 typedef enum {
     ORDER_UP,
     ORDER_DOWN,
     ORDER_INSIDE,
 } order_type;
 
+/**
+ * @brief Direction type used in @c queue_next_in_queue 
+ * and in @c queue_is_floor_ordered
+ * 
+ */
 typedef enum {
     UP,
     DOWN,
